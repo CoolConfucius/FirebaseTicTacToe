e1cbc3f059a42f9713ca5548313b7cebb8d0a13e
Benjamin Huang <coolconfucius@yahoo.com>
2016-01-20 01:42:14 -0800

    working win condition

--- app.js
+++ app.js
@@ -135,8 +135,12 @@ function updateTiles(){
     $('#t3').text(board.t3); $('#t4').text(board.t4); $('#t5').text(board.t5);
     $('#t6').text(board.t6); $('#t7').text(board.t7); $('#t8').text(board.t8);
     // $('#timer').text()
-    if (move >= 5) {
-      if (win(turn)) { alert(turn + "wins!")};
+    if (obj.move >= 5) {
+      console.log(win(turn));
+      if (win(turn)) { 
+        alert(turn + "wins!"); 
+        $('#display').text(turn+" wins!");
+      };
     };
   })
 }
@@ -146,6 +150,33 @@ function clearDom(){
   for (var i = 0; i <= 8; i++) {
     $(str+i).text('');
   };
+};
+
+var win = function(xo) {
+  console.log("win?");
+  var $t0 = $('#t0'); 
+  var $t1 = $('#t1'); 
+  var $t2 = $('#t2'); 
+  var $t3 = $('#t3'); 
+  var $t4 = $('#t4'); 
+  var $t5 = $('#t5'); 
+  var $t6 = $('#t6'); 
+  var $t7 = $('#t7'); 
+  var $t8 = $('#t8'); 
+  return(
+    check($t0, $t1, $t2, xo) || 
+    check($t3, $t4, $t5, xo) || 
+    check($t6, $t7, $t8, xo) || 
+    check($t0, $t3, $t6, xo) || 
+    check($t1, $t4, $t7, xo) || 
+    check($t2, $t5, $t8, xo) || 
+    check($t0, $t4, $t8, xo) || 
+    check($t2, $t4, $t6, xo) 
+    );
+};
+
+var check = function(t, t1, t2, xo){
+  return (t.text()===xo && t1.text()===xo && t2.text()===xo);
 };
 
 // function win( xo ){
